c60a9f0fa7749492c87f51da8938e34d13461932
Yücel Bilal <yucel.bilal@hotmail.com>
2021-01-20 23:54:04 +0300

    20 Ocak Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -6,6 +6,8 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
+            
+              
 
         }
     }
---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        // Ürün Id'si
+        public int Id { get; set; }
+        // Ürünün Kategori Id'si: hangi kategoriye dahil bir ürün olduğu bilgisi
+        public int CategoryId { get; set; }
+        // Ürün ismi
+        public string ProductName { get; set; }
+        // Ürünün Birim Fiyatı
+        public double UnitPrice { get; set; }
+        // Ürünün Stok Adedi
+        public int UnitsInStock { get; set; }
+    }
+}
